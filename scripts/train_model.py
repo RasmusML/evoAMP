a4a7436de89711826ddb543555ef1c0f38b8c746
RasmusML <rasmusml@hotmail.dk>
2024-05-17 15:27:22 +0200

    Add sequence generation notebook + default train config

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -1,25 +1,26 @@
+import os
+
 import hydra
 import pandas as pd
 import torch
 from evoamp.models import EvoAMP
 from omegaconf import OmegaConf
-import os
 
 
 @hydra.main(config_path="cfg", config_name="default_config.yaml", version_base="1.2")
 def main(config):
     print(f"configuration: \n {OmegaConf.to_yaml(config)}")
 
-    torch.manual_seed(config.seed)
+    cfg = config["experiment"]
+    torch.manual_seed(cfg.seed)
 
-    cfg_data = config["data"]
-
+    cfg_data = cfg["data"]
     df = pd.read_csv(cfg_data["dataset_path"])
-    if cfg_data["n_samples"] is not None:
+    if cfg_data["n_samples"]:
         df = df.iloc[: cfg_data["n_samples"]]
 
-    model = EvoAMP(**config["model"])
-    model.train(df, config["train"])
+    model = EvoAMP(**cfg["model"])
+    model.train(df, cfg["train"])
 
     output_dir = hydra.core.hydra_config.HydraConfig.get().runtime.output_dir
     model_dir = os.path.join(output_dir, "pretrained_model")
